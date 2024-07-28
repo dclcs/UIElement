40e0bcea605175b6865320b6439ac15f1b353eac
dclcs <dcl.outlook.1016@gmail.com>
2024-07-28 10:05:51 +0800

    Initial Commit

---
+++ UIElement/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIElement
+//
+//  Created by cl d on 2024/7/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIElement/UIElementApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIElementApp.swift
+//  UIElement
+//
+//  Created by cl d on 2024/7/28.
+//
+
+import SwiftUI
+
+@main
+struct UIElementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
